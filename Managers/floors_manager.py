a9500614db728284eac08fcbc91cb322dbd466b5
pablo <pdrm56@gmail.com>
2021-06-14 16:55:03 +0200

    refactored floor_manager

--- Managers/floors_manager.py
+++ Managers/floors_manager.py
@@ -3,6 +3,7 @@ import pymunk
 from Floors import *
 from settings.FLOORS import *
 from random import randint
+from typing import List
 # from game import Physics
 vec = pymunk.Vec2d
 
@@ -13,7 +14,7 @@ class FloorsManager(pygame.sprite.Sprite):
         :type game: game.Game
         """
         pygame.sprite.Sprite.__init__(self)
-        self.all = []
+        self.all: List[Floor] = []
         self.game = game
         self.physics = physics
 
@@ -21,61 +22,24 @@ class FloorsManager(pygame.sprite.Sprite):
         self.all = []
 
     def start(self):
-        # self.all.append(Line(self.game, position=Vec(x, y), final_pos=Vec(x, y) + Vec(512, -256), width=WIDTH))
         self.all.append(HorizontalLine(game=self.game, physics=self.physics, position=vec(*INITIALPOS), length=800,
                                        width=WIDTH))
-        # self.all.append(ParabolaDown(game=self.game, position=Vec(x, y), length=500, height=800, width=WIDTH))
-        # lastfloor: Floor = self.all[-1]
-        # self.all.append(ParabolaUp(game=self.game, position=Vec(x, y), length=600, height=-800,
-        #                          width=WIDTH))
-        # self.game.coin_manager.generate(self.all[-1])
+        self.create_horizontalline(vec(*INITIALPOS), 800)
 
     def update(self):
-        try:
-            lastfloor: Floor = self.all[-1]
-        except IndexError:
-            print(self.all)
-            raise
+        lastfloor: Floor = self.all[-1]
         distance = (lastfloor.body.position*self.game.zoom - self.game.camera.position).length
         while distance < 3000*self.game.zoom:
-            # self.all.append(HorizontalLine(game=self.game, position=lastfloor.lastpoint,
-            #                                length=800, width=WIDTH))
             selection = randint(1, 2)
             le = randint(500, 1400)
             # selection = 2
             if selection == 1:
-                if len(self.all) > 2:  # LINE
-                    h = randint(-60 * le, 80 * le) / 100
-                    if h + self.all[-2].lastpoint.y > INITIALPOS[1]:  # If you go down, do you reach de bottom?
-                        h = randint(-60 * le, 0) / 100
-                else:
-                    h = randint(-60 * le, 0) / 100
-                self.all.append(Line(game=self.game, physics=self.physics, position=lastfloor.lastpoint,
-                                     final_pos=lastfloor.lastpoint + vec(le, h), width=WIDTH))
+                self.create_line(lastfloor.lastpoint, le)
             elif selection == 2:  # PARABOLA
-                if len(self.all) > 2:
-                    h = randint(-400, 800)
-                    if h + self.all[-2].lastpoint.y > INITIALPOS[1]:  # If you go down, do you reach de bottom?
-                        h = randint(-400, 0)
-                        self.all.append(Parabola(game=self.game, physics=self.physics, position=lastfloor.lastpoint,
-                                                 length=le, height=h, width=WIDTH, up=True))
-                    else:
-                        if h >= 0:
-                            self.all.append(Parabola(game=self.game, physics=self.physics, position=lastfloor.lastpoint,
-                                                     length=le, height=h, width=WIDTH, up=False))
-                        else:
-                            self.all.append(Parabola(game=self.game, physics=self.physics, position=lastfloor.lastpoint,
-                                                     length=le, height=h, width=WIDTH, up=True))
-
-                else:
-                    # print('h')
-                    h = randint(-400, 0)
-                    self.all.append(Parabola(game=self.game, physics=self.physics, position=lastfloor.lastpoint,
-                                             length=le, height=h, width=WIDTH, up=True))
+                self.create_parabola(lastfloor.lastpoint, le)
             lastfloor: Floor = self.all[-1]
             self.game.coin_manager.generate(self.all[-1])
-            self.all.append(HorizontalLine(game=self.game, physics=self.physics, position=lastfloor.lastpoint, length=500,
-                                           width=WIDTH))
+            self.create_horizontalline(lastfloor.lastpoint, 500)
             self.game.coin_manager.generate(self.all[-1])
             lastfloor: Floor = self.all[-1]
             distance = (lastfloor.body.position - self.game.backwheel.body.position).length
@@ -83,8 +47,41 @@ class FloorsManager(pygame.sprite.Sprite):
         while len(self.all) > 30:
             self.all.pop(0)
 
+    def create_horizontalline(self, position, length):
+        self.all.append(
+            HorizontalLine(game=self.game, physics=self.physics, position=position, length=length,
+                           width=WIDTH))
+
+    def create_parabola(self, position, length):
+        if len(self.all) > 2:
+            h = randint(-400, 800)
+            if h + self.all[-2].lastpoint.y > INITIALPOS[1]:  # If you go down, do you reach de bottom?
+                h = randint(-400, 0)
+                self.all.append(Parabola(game=self.game, physics=self.physics, position=position,
+                                         length=length, height=h, width=WIDTH, up=True))
+            else:
+                if h >= 0:
+                    self.all.append(Parabola(game=self.game, physics=self.physics, position=position,
+                                             length=length, height=h, width=WIDTH, up=False))
+                else:
+                    self.all.append(Parabola(game=self.game, physics=self.physics, position=position,
+                                             length=length, height=h, width=WIDTH, up=True))
+        else:
+            # print('h')
+            h = randint(-400, 0)
+            self.all.append(Parabola(game=self.game, physics=self.physics, position=position,
+                                     length=length, height=h, width=WIDTH, up=True))
+
+    def create_line(self, position, length):
+        if len(self.all) > 2:  # LINE
+            h = randint(-60 * length, 80 * length) / 100
+            if h + self.all[-2].lastpoint.y > INITIALPOS[1]:  # If you go down, do you reach de bottom?
+                h = randint(-60 * length, 0) / 100
+        else:
+            h = randint(-60 * length, 0) / 100
+        self.all.append(Line(game=self.game, physics=self.physics, position=position,
+                             final_pos=position + vec(length, h), width=WIDTH))
+
     def draw(self):
         for floor in self.all:
             floor.draw()
-
-        # pygame.draw.circle(self.game.screen, (0, 0, 0, 255), INITIALPOS, 10)
